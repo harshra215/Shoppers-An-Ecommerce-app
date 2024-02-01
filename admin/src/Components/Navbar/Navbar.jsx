062af4bba7e6be9e0ceb87c6a8708cd5f84383af
Harsh ranjan <hr062774@gmail.com>
2024-02-01 20:46:42 +0530

    first commit

---
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css'
+import navlogo from '../../assets/nav-logo.svg'
+import navProfile from '../../assets/nav-profile.svg'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img src={navlogo} alt="" className="nav-logo" />
+      <img src={navProfile}  className='nav-profile' alt="" />
+    </div>
+  )
+}
+
+export default Navbar
---
+++ backend/index.js
@@ -0,0 +1,229 @@
+const port = 4000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+const { DESTRUCTION } = require("dns");
+
+app.use(express.json());
+app.use(cors());
+
+// Database connection with mongoDB
+mongoose.connect("mongodb+srv://Harsh123:Harsh123@cluster0.davl9x9.mongodb.net/ecommerce")
+
+// API Creation
+
+app.get("/",(req,res)=>{
+    res.send("Express App is Running")
+})
+
+// Image Storage Engine
+const storage = multer.diskStorage({
+    destination:'./upload/images',
+    filename:(req,file,cb)=>{
+        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({storage:storage})
+
+// Creating Upload Endpoint for images
+app.use('/images',express.static('upload/images'))
+
+app.post("/upload",upload.single('product'),(req,res)=>{
+    res.json({
+        success:1,
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+// Schema for Creating Products
+
+const Product = mongoose.model("Product",{
+    id:{
+        type: Number,
+        required:true,
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    new_price:{
+        type:Number,
+        required:true,
+    },
+    old_price:{
+        type:Number,
+        required:true,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    },
+    available:{
+        type:Boolean,
+        default:true,
+    },
+})
+
+app.post('/addproduct',async(req,res)=>{
+    let products = await Product.find({});
+    let id;
+    if(products.length>0)
+    {
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id=last_product.id+1;
+    }
+    else{
+        id=1;
+    }
+    const product = new Product({
+        id:id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+    });
+    console.log(product);
+    await product.save();
+    console.log("Saved");
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
+})
+
+// Creating API For deleting Products
+
+app.post('/removeproduct',async (req,res)=>{
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log("Removed");
+    res.json({
+        success:true,
+        name:req.body.name
+    })
+})
+
+// Creating API For Getting All Products
+app.get('/allproducts',async (req,res)=>{
+    let products = await Product.find({});
+    console.log("All Products Fetched");
+    res.send(products);
+})
+
+// Schema Created for user model
+const Users = mongoose.model('Users',{
+    name:{
+        type:String,
+    },
+    email:{
+        type:String,
+        unique:true,
+    },
+    password:{
+        type:String,
+    },
+    CartData:{
+        type:Object,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    }
+})
+
+//Creating Endpoint for registering the user
+app.post('/signup',async (req,res)=>{
+
+    let check = await Users.findOne({email:req.body.email});
+    if(check){
+        return res.status(400).json({success:false,errors:"existing user found with same email address"})
+    }
+    let cart = {};
+    for (let i = 0; i < 300; i++) {
+        cart[i]=0;
+    }
+    const user = new Users({
+        name:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        cartData:cart,
+    })
+
+    await user.save();
+
+    const data = {
+        user:{
+            id:user.id
+        }
+    }
+
+    const token = jwt.sign(data,'secret_ecom');
+    res.json({success:true,token})
+})
+//Creating endpoint for user login
+app.post('/login',async (req,res)=>{
+    let user = await Users.findOne({email:req.body.email});
+    if(user){
+        const passCompare = req.body.password === user.password;
+        if(passCompare){
+            const data = {
+                user:{
+                    id:user.id
+                }
+            }
+            const token = jwt.sign(data,'secret_ecom');
+            res.json({success:true,token});
+        }
+        else{
+            res.json({success:false,errors:"Wrong Password"});
+        }
+    }
+    else{
+        res.json({success:false,errors:"Wrong Email Id"})
+    }
+})
+
+// Creating endpoint for newcollection data
+app.get('/newcollections',async (req,res)=>{
+    let products = await Product.findOne({});
+    let newcollection = products.slice(1).slice(-8);
+    console.log("NewCollection Fetched");
+    res.send(newcollection);
+})
+
+// Creating endpoint for popular in women section
+app.get('/popularinwomen',async(req,res)=>{
+    let products = await Product.findOne({category:"women"});
+    let popular_in_women = products.slice(0,4);
+    console.log("Popular in women fetched");
+    res.send(popular_in_women);
+})
+   
+// Creating endpoint for products in end point
+app.post('/addtocart',async(req,res)=>{
+    console.log(req.body,req.user);
+})
+
+app.listen(port,(error)=>{
+    if(!error){
+        console.log("Server Running on Port "+port)
+    }
+    else
+    {
+        console.log("Error :"+error)
+    }
+})
